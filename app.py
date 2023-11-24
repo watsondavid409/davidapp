e789e85bea84645c473ce9e95dbcf8dc83f588d9
watsondavid409 <123076424+watsondavid409@users.noreply.github.com>
2023-11-24 16:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import time  # to simulate a real time data, time loop
 
 import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
-import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
 
 st.set_page_config(
